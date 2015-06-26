a8ce2b19218d619e9443de03b3de6562261cee02
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-26 23:50:49 +0200

    Add Xcode project

---
+++ ImageCache/Sources/ImageCache.h
@@ -0,0 +1,15 @@
+//
+//  ImageCache.h
+//  ImageCache
+//
+//  Created by Julian Grosshauser on 26/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageCache.
+FOUNDATION_EXPORT double ImageCacheVersionNumber;
+
+//! Project version string for ImageCache.
+FOUNDATION_EXPORT const unsigned char ImageCacheVersionString[];
---
+++ ImageCacheTests/Source/Classes/ImageCacheTests.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCacheTests.swift
+//  ImageCacheTests
+//
+//  Created by Julian Grosshauser on 26/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+
+class ImageCacheTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
