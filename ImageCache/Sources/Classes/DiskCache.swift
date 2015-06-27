163d1527b449cbcc6874a20e9d42b5ec743bc806
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-27 21:52:02 +0200

    Add DiskCache class

---
+++ ImageCache/Sources/Classes/DiskCache.swift
@@ -0,0 +1,14 @@
+//
+//  DiskCache.swift
+//  ImageCache
+//
+//  Created by Julian Grosshauser on 27/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+/**
+Caches data on disk asynchronously
+*/
+public class DiskCache {
+
+}
