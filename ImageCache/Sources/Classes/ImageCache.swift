aa9b8c19ea5f3380c993d593882908d55334ced4
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-27 21:54:30 +0200

    Add `init` and properties

--- ImageCache/Sources/Classes/ImageCache.swift
+++ ImageCache/Sources/Classes/ImageCache.swift
@@ -11,4 +11,22 @@ Caches images in memory and on disk
 */
 public class ImageCache {
 
+    //MARK: Properties
+
+    private let memoryCache = NSCache()
+    private let diskCache: DiskCache
+
+    /**
+    Images will be cached at this path, e.g. `Library/Caches/com.domain.App.ImageCache`
+    */
+    public let diskCachePath: String
+
+    //MARK: Initialization
+
+    public init(identifier: String) {
+        diskCache = DiskCache(identifier: identifier)
+        diskCachePath = diskCache.path
+
+        memoryCache.name = identifier
+    }
 }
