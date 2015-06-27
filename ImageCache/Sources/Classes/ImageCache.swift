45d78744f1bba48d6b5fa08f6462abe77f789e30
Julian Grosshauser <julian@juliangrosshauser.com>
2015-06-27 21:51:36 +0200

    Add ImageCache class

---
+++ ImageCache/Sources/Classes/ImageCache.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCache.swift
+//  ImageCache
+//
+//  Created by Julian Grosshauser on 27/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+/**
+Caches images in memory and on disk
+*/
+public class ImageCache {
+
+}
