72c858b9be6a13380b27bbf5d9cbc97680aba959
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-06 23:39:52 +0200

    Add `ImageCacheError` enum

---
+++ ImageCache/Sources/Enumerations/ImageCacheError.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCacheError.swift
+//  ImageCache
+//
+//  Created by Julian Grosshauser on 28/06/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+/**
+Contains all `ErrorType`s `ImageCache` can throw
+*/
+public enum ImageCacheError: ErrorType {
+
+    case EmptyKey
+    case ImageDataError
+}
