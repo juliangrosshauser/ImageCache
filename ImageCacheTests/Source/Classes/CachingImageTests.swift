7f31cb3eb5499a4dbe0f14e5a29668526a5db145
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-08 00:09:21 +0200

    Add `CachingImageTests` test case

---
+++ ImageCacheTests/Source/Classes/CachingImageTests.swift
@@ -0,0 +1,19 @@
+//
+//  CachingImageTests.swift
+//  ImageCacheTests
+//
+//  Created by Julian Grosshauser on 06/07/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+import ImageCache
+
+class CachingImageTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        clearAllCachedImages()
+    }
+}
