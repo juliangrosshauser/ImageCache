d3fb892a239e2fd6abc66ef27019a99829b35f4b
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-10 18:53:28 +0200

    Test that caching image on disk creates file

--- ImageCacheTests/Source/Classes/CachingImageTests.swift
+++ ImageCacheTests/Source/Classes/CachingImageTests.swift
@@ -63,4 +63,35 @@ class CachingImageTests: XCTestCase {
         
         waitForExpectationsWithTimeout(expectationTimeout, handler: nil)
     }
+    
+    func testCachingImageOnDiskCreatesFileInDiskCachePath() {
+        let key = "TestCachingData"
+        let image = testImageWithName("Square", fileExtension: .PNG)
+        
+        let completionExpectation = expectationWithDescription("completionHandler called")
+        
+        let completionHandler: Result<Void> -> Void = { result in
+            if case .Failure(let error) = result {
+                XCTFail("Caching image failed: \(error)")
+            }
+            
+            guard cachedImageExistsOnDiskForKey(key) else {
+                XCTFail("File wasn't created in disk cache path")
+                
+                // the following return statement will never be executed because of the above `XCTFail()`,
+                // but the Swift compiler is only happy when the `guard` body isn't falling through
+                return
+            }
+            
+            completionExpectation.fulfill()
+        }
+        
+        do {
+            try imageCache.cacheImage(image, forKey: key, onDisk: true, completionHandler: completionHandler)
+        } catch {
+            XCTFail("Caching image failed: \(error)")
+        }
+        
+        waitForExpectationsWithTimeout(expectationTimeout, handler: nil)
+    }
 }
