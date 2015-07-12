7b9f01a94167b74372b35353331b4bfcf2dae3be
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-12 23:15:43 +0200

    Test that cached image gets retrieved as expected

--- ImageCacheTests/Source/Classes/RetrievingImageTests.swift
+++ ImageCacheTests/Source/Classes/RetrievingImageTests.swift
@@ -8,10 +8,45 @@
 
 import XCTest
 import ImageCache
+import DiskCache
 
 class RetrievingImageTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
+
+        clearAllCachedImages()
+    }
+
+    func testRetrievingImageCallsCompletionHandlerWithSuccessAndExpectedImage() {
+        let key = "TestRetrievingImage"
+        let expectedImage = testImageWithName("Square", fileExtension: .PNG)
+
+        createImageInDiskCache(expectedImage, forKey: key)
+
+        let completionExpectation = expectationWithDescription("completionHandler called")
+
+        let completionHandler: Result<UIImage> -> Void = { result in
+            switch result {
+            case .Success(let image):
+                if let expectedImageData = UIImagePNGRepresentation(expectedImage), imageData = UIImagePNGRepresentation(image) {
+                    XCTAssertEqual(expectedImageData, imageData, "Retrieved image isn't equal to expected image")
+
+                    completionExpectation.fulfill()
+                } else {
+                    XCTFail("Creating image data failed")
+                }
+            case .Failure(let error):
+                XCTFail("Retrieving image failed: \(error)")
+            }
+        }
+
+        do {
+            try imageCache.retrieveImageForKey(key, completionHandler: completionHandler)
+        } catch {
+            XCTFail("Retrieving image failed: \(error)")
+        }
+
+        waitForExpectationsWithTimeout(expectationTimeout, handler: nil)
     }
 }
