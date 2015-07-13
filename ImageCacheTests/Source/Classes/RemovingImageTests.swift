4ee77ae1b659affd9adb7fed861d9aecc12d7ea5
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-13 16:53:01 +0200

    Add `RemovingImageTests` test case

---
+++ ImageCacheTests/Source/Classes/RemovingImageTests.swift
@@ -0,0 +1,17 @@
+//
+//  RemovingImageTests.swift
+//  ImageCacheTests
+//
+//  Created by Julian Grosshauser on 13/07/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+import ImageCache
+
+class RemovingImageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+}
