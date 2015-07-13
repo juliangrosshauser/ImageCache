4e9ebec692f592f17150541edd319f3a7a00bdea
Julian Grosshauser <julian@juliangrosshauser.com>
2015-07-13 21:08:14 +0200

    Test that disk cache directory gets removed

--- ImageCacheTests/Source/Classes/RemovingImageTests.swift
+++ ImageCacheTests/Source/Classes/RemovingImageTests.swift
@@ -8,10 +8,37 @@
 
 import XCTest
 import ImageCache
+import DiskCache
 
 class RemovingImageTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        
+        clearAllCachedImages()
+    }
+    
+    func testRemoveAllImagesFromDiskRemovesDiskCacheDirectory() {
+        let key = "TestRemovingAllImages"
+        let image = testImageWithName("Square", fileExtension: .PNG)
+        
+        createImageInDiskCache(image, forKey: key)
+        
+        let completionExpectation = expectationWithDescription("completionHandler called")
+        
+        let completionHandler: Result<Void> -> Void = { result in
+            if case .Failure(let error) = result {
+                XCTFail("Removing all images failed: \(error)")
+            }
+            
+            completionExpectation.fulfill()
+        }
+        
+        imageCache.removeAllImagesFromDisk(true, completionHandler: completionHandler)
+        
+        waitForExpectationsWithTimeout(expectationTimeout, handler: nil)
+        
+        XCTAssertFalse(imageCacheFileManager.fileExistsAtPath(imageCache.diskCachePath), "Disk cache directory shouldn't exist anymore")
+    }
     }
 }
